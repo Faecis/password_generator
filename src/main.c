3b881b79a68e0b6824409f47722504bde1179f04
Faecis <qkrtnals1003@naver.com>
2025-07-12 23:26:01 -0500

    Update `main.c`

--- src/main.c
+++ src/main.c
@@ -1,65 +1,11 @@
 #include <fcntl.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdint.h>
 #include <stdlib.h>
-#include <sys/random.h>
+#include <unistd.h>
 
-
-/**
- * 
- */
-char* get_string(long long length, int flags) {
-    const char** character_sets = {
-        "0123456789",                       // Numbers
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZ",       // Uppercase
-        "abcdefghijklmnopqrstuvwxyz",       // Lowercase
-        "!@#$%^&*()-_=+[]{};:,.<>/?\\|`~"   // Special
-    };
-
-    char* password = calloc(length+1, sizeof(char));
-    if (password == NULL) {
-        fprintf(stderr, "Error - Memory allocation failed.\n");
-        free(password);
-        exit(EXIT_FAILURE);
-    }
-
-    for (long long i = 0; i < length; i++) {
-        int random = 0;
-        if (getrandom(&random, sizeof(random), 0) < 0) {
-            fprintf(stderr, "Error - Failed to get random number.\n");
-            free(password);
-            exit(EXIT_FAILURE);
-        }
-
-        int mode = random % 8;
-        if () {
-
-        } else {
-            i--;
-            continue;
-        }
-    }
-}
-
-/**
- * 
- */
-void output_string(long long length, int flags, int save_fd) {
-    char* password = get_string(length, flags);
-    if (password == NULL) {
-        fprintf(stderr, "Error - Failed to generate password.\n");
-        exit(EXIT_FAILURE);
-    }
-
-    if (save_fd > 0) {
-        write(save_fd, password, length);
-    }
-    printf("%s\n", password);
-
-    free(password);
-
-    return;
-}
+#include "generator.h"
 
 
 int main(int argc, char** argv) {
@@ -67,37 +13,36 @@ int main(int argc, char** argv) {
 
     // Check if there are the correct number of arguments.
 
-    if (argc < 3 || argc > 4) {
-        fprintf(stderr, "Error - Invalid number of arguments.\n");
-        fprintf(stderr, "Error - Usage: %s <length> <flag>\n", argv[0]);
-        fprintf(stderr, "Error - Usage: %s <length> <flag> <output_file>\n", argv[0]);
+    if (argc < 4 || argc > 5) {
+        fprintf(stderr, "ERROR - Invalid number of arguments.\n");
+        fprintf(stderr, "ERROR - Usage: %s <length> <repeat> <flag>\n", argv[0]);
+        fprintf(stderr, "ERROR - Usage: %s <length> <repeat> <flag> <output_file>\n", argv[0]);
         return EXIT_FAILURE;
     }
 
-    // Parse the length of the password.
+    // Parse the length of the password and the number of repetitions.
 
-    long long length = atoll(argv[1]);
-    if (length <= 0) {
-        fprintf(stderr, "Error - Invalid length: %s.\n", argv[1]);
-        return EXIT_FAILURE;
-    } else if (length > (1LL << sizeof(long long))-1) {
-        fprintf(stderr, "Error - Length too large: %s.\n", argv[1]);
-        fprintf(stderr, "Error - Maximum length: %lld\n", (1LL<<sizeof(long long))-1);
-        return EXIT_FAILURE;
-    }
-    fprintf(stdout, "Password Length: %d\n", length);
+    uint64_t length = (uint64_t)atoll(argv[1]);
+    uint64_t repeat = (uint64_t)atoll(argv[2]);
+
+    fprintf(stdout, "Password Length: %ld\n", length);
+    fprintf(stdout, "Number of Repetitions: %ld\n", repeat);
 
     // Parse the flags for the password generation.
 
-    int flags = 0;
+    uint8_t flags = 0;
     for (int i = 0; i < 4; i++) {
-        if (argv[2][i] == '1') {
-            flags |= (1 << 3-i);
-        } else if (argv[2][i] != '0') {
-            fprintf(stderr, "Error - Invalid flag: %s.\n", argv[1]);
+        if (argv[3][i] == '1') {
+            flags |= 1 << (3-i);
+        } else if (argv[3][i] != '0') {
+            fprintf(stderr, "ERROR - Invalid flag: %s.\n", argv[3]);
             return EXIT_FAILURE;
         }
+    }
 
+    // Report the parsed flags.
+
+    for (int i = 0; i < 4; i++) {
         switch (i) {
             case 0:
                 fprintf(stdout, "Numbers: %s\n", (flags & 0b1000) ? "Enabled" : "Disabled");
@@ -119,10 +64,10 @@ int main(int argc, char** argv) {
     // Open the output file if provided.
 
     int save_fd = 0;
-    if (argc == 4) {
-        save_fd = open(argv[3], O_WRONLY | O_CREAT | O_TRUNC);
+    if (argc == 5) {
+        save_fd = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC);
         if (save_fd < 0) {
-            fprintf(stderr, "Error - Opening output file: %s.\n", argv[3]);
+            fprintf(stderr, "ERROR - Opening output file: %s.\n", argv[4]);
             return EXIT_FAILURE;
         }
         fprintf(stdout, "Output File: %s\n", argv[3]);
@@ -133,8 +78,10 @@ int main(int argc, char** argv) {
 
     // Generate and output the password.
 
-    output_string();
-    close(save_fd);
+    generate(length, repeat, flags, save_fd);
+    fprintf(stdout, "\n");
+
+    if (save_fd > 0) close(save_fd);
 
     fprintf(stdout, "Password generated.\n");
 
